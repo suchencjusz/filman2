fcb18b53badd275af1ccf37c841fdc276d2c2f74
kuba <sucheta348@gmail.com>
2024-09-21 13:26:16 +0200

    fix: fixed discord embed

--- src/filman_crawler/tasks/scrap_user_watched_movies.py
+++ src/filman_crawler/tasks/scrap_user_watched_movies.py
@@ -62,7 +62,7 @@ class Scraper:
         first_time_scrap = True if user_already_watched_ids == [] else False
         user_already_watched_ids = set(user_already_watched_ids or [])
 
-        new_movies_watched = [movie for movie in last_100_watched_data if movie[0] not in user_already_watched_ids]
+        new_movies_watched = [movie for movie in last_100_watched_data if movie[0] not in user_already_watched_ids] # something is fucked up here 
         new_movies_watched_parsed = []
 
         logging.debug(f"Found {len(new_movies_watched)} new movies watched")
